045d1416003966766c33691c608514c0547288cf
Carlos Peix <carlos.peix@gmail.com>
2013-12-08 16:14:03 -0300

    No es necesario que las variables sean globales

--- proyector/features/step_definitions/player_steps.rb
+++ proyector/features/step_definitions/player_steps.rb
@@ -2,23 +2,23 @@ require File.join(File.dirname(__FILE__),'../../lib/proyector')
 require File.join(File.dirname(__FILE__),'../../lib/timer_test')
 
 Given(/^El proyector esta encendido$/) do
-	@@timer = TimerTest.new
-	@@proyector = Proyector.new(@@timer)
-	@@proyector.encender
+	@timer = TimerTest.new
+	@proyector = Proyector.new(@timer)
+	@proyector.encender
 end
 
 When(/^Apago el proyector$/) do
-	@@proyector.apagar
+	@proyector.apagar
 end
 
 Then(/^el ventilador esta encendido$/) do
-	@@proyector.ventilador_encendido.should be true
+	@proyector.ventilador_encendido.should be true
 end
 
 When(/^pasan (\d+) segundos$/) do |segundos|
-	@@timer.avanzar_segundos(segundos.to_i)
+	@timer.avanzar_segundos(segundos.to_i)
 end
 
 Then(/^el ventilador esta apagado$/) do
-	@@proyector.ventilador_encendido.should be false
+	@proyector.ventilador_encendido.should be false
 end
